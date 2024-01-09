39925875eaa07f7eae1cecfc34c1a229e3e29173
Rares <126565978+stefearares@users.noreply.github.com>
2024-01-09 18:13:54 +0200

    Update Main.java

--- src/main/java/employeepack/Main.java
+++ src/main/java/employeepack/Main.java
@@ -1,8 +1,10 @@
 package employeepack;
 
+import static employeepack.Functions.createTable;
+
 public class Main {
     public static void main(String[] args) {
-
+            createTable();
             GUI gui=new GUI();
             gui.setVisible(true);
             gui.setLocationRelativeTo(null);
